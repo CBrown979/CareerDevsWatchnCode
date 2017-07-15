e3192155ab9195059bc4313f8fc176a922ca5a59
Candice Bianca Brown <candicebrown97@gmail.com>
2017-07-15 23:38:25 +0000

    version5-loopsOfLogic completed

--- todos.js
+++ todos.js
@@ -1,24 +1,64 @@
 //Version 5 - Loops of Logic
 
+//Initialization - creating a variable to keep track of how many times you want to do something
+//Condition - if this is true, keep going - otherwise STOP
+//Final Expression - this happens after each round
+//Structure - for(initialization; condition; final-expression) {
+//  do some action -- such as console.log("hey");
+//}
+
 // var testArray=["item 1", "item 2", "item 3"];
 
-// for (var i=0; i<testArray.length; i++) {
+// for (var i=0; i<testArray.length; i++) {  
 //     //console.log("Hey");
 //     //console.log(i);
 //     console.log(testArray[i]);
-// }
+// } Note: i++ is the same as i=i+1
 
 var todoList = {
   todos: [],
   displayTodos: function() {
-    console.log("My Todos:", this.todos);
-    //for (var i=0; i<this.todos.length; i++) {
-    //example: this.todos.length has 3 items
+    if (this.todos.length === 0) {
+      console.log("Your todo list is empty!");
+    } else {
+      console.log("My Todos:");
+      for (var i=0; i<this.todos.length; i++) {
+        if (this.todos[i].completed === true) {
+            console.log("(x)".this.todos[i].todoText);
+      }     else {
+            console.log("( )".todos[i].todoText);
+      }
+    }
+    //To show .todoText: example: the array is this.todos, and this.todos.length has 3 items
     //i=0
     //i=1
     //i=2
-    //this.todos[i].todoText; -- will look at each item in the array, but want the todoText property on the array
+    //this.todos[i] is equal to each item in the array
+    //this.todos[i].todoText is equal to the todoText property on each in the array
   //}
+    //pseudo code for telling if .todos is empty
+      //if this.todos.length === 0 --> === is the strictest, most consistent value comparison in JS
+        //console.log("Your todoList is empty!");
+      //else
+        //print todos as normal --> (with displayTodos forLoop) 
+    
+    //   if (this.todos.length === 0) {
+    //   console.log("Your todolist is empty!");
+    // }   else {
+    //     console.log("My Todos:");
+    //     for (var i=0; i<this.todos.length; i++) {
+    //       if this.todos[i].completed === true {
+    //         console.log("(x)".todos[i].todoText);
+    //   }     else {
+    //           console.log("( )".todos[i].todoText);
+    //   }
+      
+      //psuedo code for show .completed
+      // check if .completed is true
+        // print with (x)
+        // else
+          //print with ( )
+    }
   },
   addTodo: function(todoText) {//this todoText is the name of the property on this object
     this.todos.push({
