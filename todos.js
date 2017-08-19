b7da443e3cf9c3ba339078e4c387101494f6de4e
Candice Bianca Brown <candicebrown97@gmail.com>
2017-08-19 08:42:41 +0000

    chapter 9 complete for submission

--- todos.js
+++ todos.js
@@ -1,43 +1,23 @@
-//Version 8 - Getting Data From Inputs
-//refactoring - restructuring process of existing code (to make more readable, etc) w/o changing external behavior
+//Version 9 - Escape from the Console
 
 var todoList = {
   todos: [],
-  displayTodos: function() {
-    debugger; //once done with debugger, delete this line from code
-    if (this.todos.length === 0) {
-      console.log("Your todo list is empty!");
-    } else {
-      console.log("My Todos:");
-      for (var i=0; i<this.todos.length; i++) {
-        if (this.todos[i].completed === true) {
-            console.log("(x)".this.todos[i].todoText);
-      }     else {
-            console.log("( )".todos[i].todoText);
-      }
-    }
-    }
-  },
   addTodo: function(todoText) {//this todoText is the name of the property on this object
     this.todos.push({
       todoText: todoText, //this refers to the todoText that is a parameter and can change based on what you pass in when you use the function
       completed: false
-    }); //we want our todos to show alittle more data, and want a text description - but also want another property (2 total) to see if the todo has been completed or not
-    this.displayTodos();
+    }); //we want our todos to show alittle more data, and want a text description - but also want another property (2 total) to see if theTodo has been completed or not
   },
   changeTodo: function(position, todoText) { //todoText was added to make it clearer that we are changing just the todoText property and not the entire object
     //this.todos[position]=newValue; (this was less clear than above, hence revised to above)
-    this.todos[position].todoText=todoText; //used .notation to grab just the todoText property, and once we had that we set it to the new value that is passed in when you run the function
-    this.displayTodos();
+    this.todos[position].todoText = todoText; //used .notation to grab just the todoText property, and once we had that we set it to the new value that is passed in when you run the function
   },
   deleteTodo: function(position) {
     this.todos.splice(position, 1);
-    this.displayTodos();
   },
   toggleCompleted: function(position) {
-      var todo = this.todos[position];//grabbing and saving the specific todo we're interested in
-      todo.completed = !todo.completed;//grabbing todo.completed (which is a boolean true/false value), and we want to change the value of it to the opposite of what it is - 1)get todo.completed, put on right side and add the bang operator (!) before it to flip it to opposite of what it currently is     
-      this.displayTodos();
+      var todo = this.todos[position];//grabbing and saving the specificTodo we're interested in
+      todo.completed = !todo.completed;//grabbing todoDotcompleted (which is a boolean true/false value), and we want to change the value of it to the opposite of what it is - 1)get todo.completed, put on right side and add the bang operator (!) before it to flip it to opposite of what it currently is     
   },
   toggleAll: function() { // we're recording the total number of todos and completed todos to see if they are equal
     var totalTodos = this.todos.length;
@@ -54,65 +34,79 @@ var todoList = {
     // Case 1: If everything is true, make everything false.
     if (completedTodos === totalTodos) { // we are checking to make sure that everything is true
       // Make everything false.
-      for(var i=0; i<totalTodos; i++) { // we're going thru all the items and changing all completed items to false
-        this.todos[i].completed=false;
+      for(var i = 0; i < totalTodos; i++) { // we're going thru all the items and changing all completed items to false
+        this.todos[i].completed = false;
       }
     // Case 2: Otherwise, make everything true.  
     } else {
       for( var i=0; i<totalTodos; i++) {
-        this.todos[i].completed=true;
+        this.todos[i].completed = true;
       }
     }
-    this.displayTodos();
   }
   
 };
-
-// 1) We want to get access to the display todos button via the getElementID
-// var displayTodosButton=document.getElementById("displayTodosbutton");
-// var toggleAllButton=document.getElementById("toggleAllButton");
-// // New refactored code on index.html -- does the same as above but no longer need IDs - using onclick
-
-// 2) We want to run displayTodos method, when someone clicks the display todos button
-// displayTodosButton.addEventListener("click", function() {
-//   todoList.displayTodos();  
-// });
-
-// toggleAllButton.addEventListener("click", function() {
-//     todoList.toggleAll();
-// });
-
 var handlers = {//we want the methods on this object to handle different events or clicks - when clicked, we want a handler to handle that
-  displayTodos: function() {
-    todoList.displayTodos();
-  },
-  toggleAll: function() {
-    todoList.toggleAll();
-  },
   addTodo: function() {
     var addTodoTextInput = document.getElementById("addTodoTextInput");
     todoList.addTodo(addTodoTextInput.value);
     addTodoTextInput.value="";
+    view.displayTodos();
   },
   changeTodo: function() {
     var changeTodoPositionInput = document.getElementById("changeTodoPositionInput");
     var changeTodoTextInput = document.getElementById("changeTodoTextInput");
-    todoList.changeTodo;
+    todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
     changeTodoPositionInput.value="";
     changeTodoTextInput.value="";
-    
+    view.displayTodos();
   },
   deleteTodo: function() {
     var deleteTodoPositionInput = document.getElementById("deleteTodoPositionInput");
     todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
     deleteTodoPositionInput.value = "";
+    view.displayTodos();
   },
   toggleCompleted: function() {
     var toggleCompletedPositionInput = document.getElementById("toggleCompleted");
     todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
     toggleCompletedPositionInput.value = "";
+    view.displayTodos();
   },
   toggleAll: function() {
     todoList.toggleAll();
+    view.displayTodos();
+  }
+};
+
+var view = {
+  displayTodos: function () {
+    var todosUl = document.querySelector('ul');
+    todosUl.innerHTML = '';
+    for (var i = 0; i < todoList.todos.length; i++) {
+      var todoLi = document.createElement('li');
+      var todo = todoList.todos[i]; //the [i] is needed to grab each particular todoFrom the list
+      
+      var todoTextWithCompletion = '';
+      if (todo.completed === true) {
+        todoTextWithCompletion = '(x)' + todo.todoText;
+      } else {
+        todoTextWithCompletion = '( )' + todo.todoText;
+      }
+      
+      todoLi.textContent = todoTextWithCompletion; // this line will now replace todoLi.textContent=todoList.todos[i].todoText;
+            //todoLi.textContent = todoList.todos[i].todoText; //the textContent property on these elements can be changed by setting its value to the todoText property of each todoObject inside todosArray
+      todosUl.appendChild(todoLi);
+    }
+      
+      // Comments for above
+      //'(x) todoText' - structure of the string we want on the screen - with an '(x)' if completed followed by the todoText
+      // to do this, we need to create a new variable to store this, and will change whether the completed property is true or false
+      // var todoTextWithCompletion = ''; - initial set to nothing - will change same with an if statement, as follows:
+          // if (todoDotcompleted === true)
+            // (x) todoText
+          // else
+            // ( ) todoText
+          // todoLi.textContent = todoTextWithCompletion;
   }
 };
